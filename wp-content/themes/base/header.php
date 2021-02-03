80aba0abcf7d5576f3af98faf075755b33764309
maka2004 <maka2004@rambler.ru>
2021-02-03 11:24:22 +0200

    header; favicon

---
+++ wp-content/themes/base/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+<head>
+
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" type="image/x-icon">
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php
+wp_body_open();
+?>
+
+<header id="site-header" class="header-footer-group" role="banner">
+
+</header><!-- #site-header -->
+
+<?php
